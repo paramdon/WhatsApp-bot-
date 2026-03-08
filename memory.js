5cd6b2c3cdd983666728aea38d8f86d06c6f60d1
paramdon <143490521+paramdon@users.noreply.github.com>
2026-03-08 13:25:07 +0530

    Update memory.js

--- memory.js
+++ memory.js
@@ -1,15 +1,28 @@
 const fs = require("fs")
+const path = require("path")
 
-function loadMemory(user){
-    try {
-        return JSON.parse(fs.readFileSync(`./memory/${user}.json`))
-    } catch {
-        return { messages: [] }
-    }
+const MEMORY_DIR = path.join(__dirname, "memory")
+
+if (!fs.existsSync(MEMORY_DIR)) {
+  fs.mkdirSync(MEMORY_DIR)
 }
 
-function saveMemory(user, data){
-    fs.writeFileSync(`./memory/${user}.json`, JSON.stringify(data))
+function loadMemory(user) {
+
+  const file = path.join(MEMORY_DIR, user + ".json")
+
+  try {
+    return JSON.parse(fs.readFileSync(file))
+  } catch {
+    return {}
+  }
+}
+
+function saveMemory(user, data) {
+
+  const file = path.join(MEMORY_DIR, user + ".json")
+
+  fs.writeFileSync(file, JSON.stringify(data, null, 2))
 }
 
 module.exports = { loadMemory, saveMemory }
